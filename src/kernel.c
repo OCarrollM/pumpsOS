062ee824923e86616bc8240048a1cd4fbbd1bfc9
ocarrolm <morganocarroll01@gmail.com>
2025-11-14 00:48:19 +0000

    QEMU menu, needs multiboot though

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,7 @@
 // src/kernal.c
 #include <stdio.h>
 #include <stddef.h>
+#define COM1 0x3F8
 // Awesome sauce from Paul Dempster 2021
 
 // VGA Text Buffer
@@ -11,19 +12,20 @@ static inline __uint16_t vga_entry(unsigned char ch, __uint8_t color) {
     return (__uint16_t)ch | (__uint16_t)color << 8;
 }
 
+static inline void serial_write_char(char c) { 
+    while(!(*(volatile unsigned char *)(COM1 + 5) & 0x20 ));
+    *(volatile unsigned char *)(COM1) = c;
+}
+
+static void serial_write(const char* str) {
+    for(size_t i=0; str[i]; i++) {
+        serial_write_char(str[i]);
+    }
+}
+
 void kernel_main(void) {
     const char *msg = "I love Pumps, I also love baby flo but this is pumps tiny kernal";
-    size_t i = 0;
-    // eliminate the first line
-    for (i=0; i<VGA_WIDTH; i++) {
-        vga_buffer[i] = vga_entry(' ', 0x07);
-    }
-    // Write some message lol
-    i = 0;
-    while(msg[i]) {
-        vga_buffer[i] = vga_entry(msg[i], 0x0F); // white on black should be orange though
-        i++;
-    }
+    serial_write(msg);
     // simple loop
     for(;;) { __asm__ ("hlt"); }
 }
